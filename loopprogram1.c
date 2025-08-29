30f57313784cd1ec5028078683a096dd6efdd8e3
vamjafenil2006-prog <vamjafenil2006@gmail.com>
2025-08-29 10:13:27 +0530

    Add files via upload

---
+++ loopprogram1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n, i=0;
+do
+{
+i++;
+printf("%d\n" , i);
+}
+while (i<10);
+return 0;
+}
---
+++ loopprogram5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int n, i=0;
+printf("enter the value of n:");
+scanf("%d", &n);
+do
+{
+i++;
+printf("%d\n" , 2*i-1);
+}
+while (i<n);
+return 0;
+}
