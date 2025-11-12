f79784a69fb44ddbb9c333149efb0d878a09038a
vamjafenil2006-prog <vamjafenil2006@gmail.com>
2025-11-12 11:03:14 +0530

    Add files via upload

---
+++ Basic_Q02.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,sub;
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("Enter second number:");
+    scanf("%d",&b);
+    sub = a - b;
+    printf("Subtraction = %d",sub);
+    return 0;
+}
