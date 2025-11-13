789294330c8c92bccab755451ff89b92a1828856
vamjafenil2006-prog <vamjafenil2006@gmail.com>
2025-11-13 15:08:12 +0530

    Add files via upload

---
+++ Basic_01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int num1,num2,sum;
+ printf("the value of num1: ");
+ scanf("%d",&num1);
+
+ printf("the value of num2: ");
+ scanf("%d",&num2);
+
+ sum= num1+num2;
+ printf("The sum of num1 and num2 is %d",sum);
+
+ return 0;
+
+}
---
+++ Basic_02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, difference;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    difference = num1 - num2;
+    printf("Difference is: %d\n", difference);
+    return 0;
+}
